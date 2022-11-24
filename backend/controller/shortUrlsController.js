1cc9af3e99970fd41fa92988b4a756c8861c4d87
laophan74 <laophan74@gmail.com>
2022-11-24 15:50:58 +0700

    url shortener

--- backend/controller/shortUrlsController.js
+++ backend/controller/shortUrlsController.js
@@ -1,4 +1,4 @@
-const { ShortUrl } = require('../database/shortUrl');
+const { ShortUrl, Account } = require('../database/shortUrl');
 
 async function shortUrlsController(req, res) {
   try {
@@ -14,14 +14,22 @@ async function shortUrlsController(req, res) {
 
 async function deleteUrlsController(req, res) {
   try {
-    if (req.body.short) {
-      await ShortUrl.deleteOne({ short: req.body.short });
-      // res.status(201).json('Success');
+    if (req.body._id) {
+      // await ShortUrl.deleteOne({ _id: req.body._id });
+      const user = await Account.findOne({ urls: req.body._id });
+      const index = user.urls.indexOf(req.body._id);
+      const urls = user.urls[0];
+      // const result = await Account.updateOne(
+      //   { _id: user._id },
+      //   { urls },
+      // );
+      res.send(`${index}`);
+      // res.status(201).json(_id);
     }
   } catch (error) {
     res.status(500).json('Failed');
   }
-  res.redirect('http://localhost:3000/');
+  // res.redirect('http://localhost:3000/');
 }
 
 module.exports = { shortUrlsController, deleteUrlsController };
